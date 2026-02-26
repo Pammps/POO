38bdb4d0427791eb801da23c3cbf11a9408a8c65
Pammps <pamellasouza331@gmail.com>
2026-02-26 11:47:05 -0300

    aula 2

--- src/br/com/fiapride/model/Projetor.java
+++ src/br/com/fiapride/model/Projetor.java
@@ -1,10 +1,41 @@
 package br.com.fiapride.model;
 
 public class Projetor {
-
+	
 	// atributos (caracteristicas)
 	public int peso;
 	public double tamanho;
-	public String tipo_fonte;
+	public String tipoFonteLampada;
+	public String tipoFonteLed;
+	public boolean ligado;
+	
+	public Projetor(int peso, double tamanho, String tipo_fonte, boolean ligado) {
+		this.peso = peso;
+		this.tamanho = tamanho;
+		this.tipoFonteLampada = tipoFonteLampada;
+		this.tipoFonteLed = tipoFonteLampada;
+		this.ligado = ligado;
+	}
 
 }
+
+	public void projetarImagem(String imagem) {
+		if (ligado) {
+			System.out.println("Imagem sendo projetada");
+		} else {
+			System.out.println("Projetor está desligado");
+		return;
+		}
+		if (tipoFonteLampada == tipoFonteLampada) {
+			System.out.println("Projetando com lâmpada amarela"); 
+				
+		} else if (tipoFonteLed == tipoFonteLed) {
+			System.out.println("Projetando com LED");
+		}
+		
+		System.out.println(Imagem + imagem);
+		}
+	
+	public Void emitirSom(double som) {
+		if (s)
+	}
