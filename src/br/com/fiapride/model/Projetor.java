702f1e6fa467a277c9630c72b1455b952b714308
Pamella Ferreira <PAMELLASOUZA331@GMAIL.COM>
2026-03-12 22:08:32 -0300

    aula 3 completa

--- src/br/com/fiapride/model/Projetor.java
+++ src/br/com/fiapride/model/Projetor.java
@@ -3,10 +3,10 @@ package br.com.fiapride.model;
 public class Projetor {
 
 	// atributos
-	public int peso;
-	public double tamanho;
-	public String tipoFonte;
-	public boolean ligado;
+	private int peso;
+	private double tamanho;
+	private String tipoFonte;
+	private boolean ligado;
 
 	// construtor
 	public Projetor(int peso, double tamanho, String tipoFonte) {
@@ -16,9 +16,50 @@ public class Projetor {
 		this.ligado = false;
 	}
 
-	// método 1 - ligar projetor
+	// GETTERS
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public double getTamanho() {
+		return tamanho;
+	}
+
+	public String getTipoFonte() {
+		return tipoFonte;
+	}
+
+	public boolean isLigado() {
+		return ligado;
+	}
+
+	// SETTERS
+
+	public void setPeso(int peso) {
+
+		// REGRA DE NEGÓCIO
+		if (peso <= 0) {
+			System.out.println("Peso inválido!");
+			return;
+		}
+
+		this.peso = peso;
+	}
+
+	public void setTamanho(double tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public void setTipoFonte(String tipoFonte) {
+		this.tipoFonte = tipoFonte;
+	}
+
+	// MÉTODOS
+
 	public void ligar() {
-		if (ligado == true) {
+
+		if (ligado) {
 			System.out.println("O projetor já está ligado.");
 		} else {
 			ligado = true;
@@ -26,9 +67,9 @@ public class Projetor {
 		}
 	}
 
-	// método 2 - desligar projetor
 	public void desligar() {
-		if (ligado == false) {
+
+		if (!ligado) {
 			System.out.println("O projetor já está desligado.");
 		} else {
 			ligado = false;
@@ -36,20 +77,13 @@ public class Projetor {
 		}
 	}
 
-	// método 3 - projetar imagem
 	public void projetarImagem(String imagem) {
 
-		if (ligado == false) {
+		if (!ligado) {
 			System.out.println("Não é possível projetar. O projetor está desligado.");
 			return;
 		}
 
-		if (tipoFonte.equals("Lâmpada")) {
-			System.out.println("Projetando com lâmpada amarela...");
-		} else if (tipoFonte.equals("LED")) {
-			System.out.println("Projetando com LED...");
-		}
-
-		System.out.println("Imagem projetada: " + imagem);
+		System.out.println("Projetando imagem: " + imagem);
 	}
 }
